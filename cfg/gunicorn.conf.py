677ed98d47799953cb4ce808c097313cdc3c750f
Arnaud Rocher <arnaud.roche3@gmail.com>
2014-11-02 00:51:26 +0100

    Add gunicorn configuration file

---
+++ cfg/gunicorn.conf.py
@@ -0,0 +1,8 @@
+# Gunicorn configuration file
+# Goes in : stays here !
+
+import multiprocessing
+
+proc_name = 'home_web'
+workers = multiprocessing.cpu_count() * 2 + 1
+bind = 'unix:/tmp/home_web_gunicorn.sock'
