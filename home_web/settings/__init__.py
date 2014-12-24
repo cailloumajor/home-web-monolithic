90055c709602943d284eb6971f4261a3c0892333
Arnaud Rocher <arnaud.roche3@gmail.com>
2014-12-24 15:26:59 +0100

    Modify django settings to collect static files on development machine

--- home_web/settings/__init__.py
+++ home_web/settings/__init__.py
@@ -1,5 +1,11 @@
+#-*- coding: utf-8 -*-
+
+import os
+
 from .development import *
 from .external_config import yj_present
 
-if yj_present:
+if os.environ.get('DJANGO_CONFIG_PARAM') == 'static_build':
+    from .static_build import *
+elif yj_present:
     from .production import *
