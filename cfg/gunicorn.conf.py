ed55ed1fbbed553f9e4c146d1f15cdc96b8911cd
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-01-10 19:04:41 +0100

    Add gunicorn pidfile setting

--- cfg/gunicorn.conf.py
+++ cfg/gunicorn.conf.py
@@ -3,4 +3,5 @@ import multiprocessing
 proc_name = 'home_web'
 workers = multiprocessing.cpu_count() * 2 + 1
 bind = 'unix:/tmp/home_web_gunicorn.sock'
+pidfile = '/run/gunicorn/home_web.pid'
 errorlog = '-'
