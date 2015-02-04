924c3a67f11e8b03d2b36a57bc39120cd4ad7fe1
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-02-04 22:29:23 +0100

    Add a shell script for launching Gunicorn

--- cfg/gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-import multiprocessing
-
-proc_name = 'home_web'
-workers = multiprocessing.cpu_count() * 2 + 1
-bind = 'unix:/tmp/home_web_gunicorn.sock'
-pidfile = '/run/gunicorn/home_web.pid'
-errorlog = '-'
