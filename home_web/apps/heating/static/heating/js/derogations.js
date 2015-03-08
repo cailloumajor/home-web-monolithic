3bd3a8929eaa8ab94e04e44dadf7775bd793f76e
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-03-08 15:28:10 +0100

    Separate derogation list arrangement javascript

--- home_web/apps/heating/static/heating/js/derogations.js
+++ home_web/apps/heating/static/heating/js/derogations.js
@@ -3,11 +3,15 @@ define(
 function($) {
     return {
         init: function() {
+            var self = this;
             $('#derogation-list').addClass(
                 'ui-widget ui-widget-content ui-corner-all derogation-js'
             ).children('h2').addClass(
                 'ui-widget-header ui-corner-all'
             ).children('a').button();
+            this.arrange();
+        },
+        arrange: function() {
             $('#derogation-list .urls a').button();
             $(".derog-check:contains('X')").addClass('ui-icon ui-icon-check');
         },
