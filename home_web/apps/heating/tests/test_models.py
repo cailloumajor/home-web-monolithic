2734e052aed082128732ea90aec3dc2454abb89c
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-03-24 19:07:25 +0100

    Add models string representation tests

---
+++ home_web/apps/heating/tests/test_models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from django.test import TestCase
+
+from ..models import Zone, Slot
+
+class ZoneModelTest(TestCase):
+
+    def test_string_representation(self):
+        zone = Zone(num=1)
+        self.assertEqual(str(zone), 'Z{}'.format(zone.num))
+
+class SlotModelTest(TestCase):
+
+    def test_string_representation(self):
+        zone = Zone(num=1)
+        slot = Slot(
+            zone=zone, mon=True, wed=True, fri=True, sun=True,
+            start_time = datetime.time(4,2), end_time = datetime.time(15,54),
+            mode = 'E'
+        )
+        self.assertEqual(str(slot), "Z1 04:02:00-15:54:00 [L*M*V*D] Eco")
+
