a390336a44a19105a2ef3b723561109f718123f2
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-03-26 22:25:25 +0100

    Add tags to ignore parts from coverage measuring

--- home_web/settings/__init__.py
+++ home_web/settings/__init__.py
@@ -5,7 +5,7 @@ import os
 from .development import *
 from .external_config import yj_present
 
-if os.environ.get('DJANGO_CONFIG_PARAM') == 'static_build':
+if os.environ.get('DJANGO_CONFIG_PARAM') == 'static_build': # pragma: no cover
     from .static_build import *
-elif yj_present:
+elif yj_present: # pragma: no cover
     from .production import *
