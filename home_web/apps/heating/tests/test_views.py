d8fa4beec0eeff4323e2b6d449a8f4c32287b728
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-03-30 22:13:08 +0200

    Add tests for Ajax response views

--- home_web/apps/heating/tests/test_views.py
+++ home_web/apps/heating/tests/test_views.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 import datetime
+import json
 import random
 
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.forms.models import model_to_dict
+from django.core.serializers.json import DjangoJSONEncoder
 
 from ..models import Zone, Slot
 
@@ -55,3 +60,59 @@ class SlotViewsTest(TestCase):
             reverse('del_slot', kwargs={'pk':self._slot.pk})
         )
         self.assertEqual(response.status_code, 200)
+
+class AjaxSlotViewsTest(TestCase):
+
+    longMessage = True
+
+    def test_ajax_slot_create_view_form_invalid(self):
+        url = reverse('new_slot', kwargs={'zone':1})
+        response = self.client.post(url, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        response_non_ajax = self.client.post(url)
+        self.assertIsInstance(json.loads(response.content), dict)
+        self.assertEqual(json.loads(response.content),
+                         response_non_ajax.context['form'].errors)
+
+    def test_ajax_slot_create_view_form_valid(self):
+        z = Zone.objects.create(num=1, desc="Test zone")
+        response = self.client.post(
+            reverse('new_slot', kwargs={'zone':z.num}),
+            {
+                'zone': z.num, 'mon': True,
+                'start_time': '04:30',
+                'end_time': '06:30',
+                'mode': 'E'
+            },
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+        )
+        self.assertIsInstance(json.loads(response.content), dict)
+        created_slot = Slot.objects.all()[0]
+        model_dict = model_to_dict(created_slot)
+        response_dict = json.loads(response.content)
+        for key in model_dict:
+            self.assertEqual(
+                response_dict.get(key),
+                json.loads(json.dumps(model_dict[key], cls=DjangoJSONEncoder)),
+                msg="Field: {}".format(key)
+            )
+        self.assertEqual(response_dict.get('django_success'),
+                         True)
+        self.assertEqual(response_dict.get('pk'),
+                         'pk-{}'.format(created_slot.pk))
+        self.assertEqual(response_dict.get('addUpdURL'),
+                         reverse('update_slot', kwargs={'pk':created_slot.pk}))
+        self.assertEqual(response_dict.get('delURL'),
+                         reverse('del_slot', kwargs={'pk':created_slot.pk}))
+
+    def test_ajax_slot_delete_view(self):
+        z = Zone.objects.create(num=1, desc="Test zone")
+        s = Slot.objects.create(
+            zone=z, mon=True, mode='E',
+            start_time=datetime.time(4), end_time=datetime.time(6),
+        )
+        self.assertEqual(Slot.objects.all().get(), s)
+        response = self.client.post(reverse('del_slot', kwargs={'pk':s.pk}),
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertIsInstance(json.loads(response.content), dict)
+        self.assertEqual(json.loads(response.content).get('django_success'),
+                         True)
