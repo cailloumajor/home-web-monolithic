8afa56a1e33fd51c113f34dbfc0d0f658da85027
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-06-02 18:41:06 +0200

    Add a Derogation model string representation test

--- home_web/apps/heating/tests/test_models.py
+++ home_web/apps/heating/tests/test_models.py
@@ -3,8 +3,9 @@
 import datetime
 
 from django.test import TestCase
+from django.utils import timezone
 
-from ..models import Zone, Slot
+from ..models import Zone, Slot, Derogation
 
 class ZoneModelTest(TestCase):
 
@@ -23,3 +24,18 @@ class SlotModelTest(TestCase):
         )
         self.assertEqual(str(slot), "Z1 04:02:00-15:54:00 [L*M*V*D] Eco")
 
+class DerogationModelTest(TestCase):
+
+    def test_string_representation(self):
+        zone2 = Zone.objects.create(num=2)
+        zone3 = Zone.objects.create(num=3)
+        tz = timezone.get_default_timezone()
+        start = timezone.make_aware(
+            datetime.datetime(2015, 2, 25, 17, 24), tz
+        )
+        end = timezone.make_aware(
+            datetime.datetime(2015, 3, 18, 18, 12), tz
+        )
+        derog = Derogation.objects.create(start_dt=start, end_dt=end, mode='H')
+        derog.zones = [zone2, zone3]
+        self.assertEqual(str(derog), "25/02-17:24->18/03-18:12 H Z2-Z3")
