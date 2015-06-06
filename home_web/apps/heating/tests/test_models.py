f66ae3a46961ae2e7927ea95921a455c53999556
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-06-06 15:10:50 +0200

    Add a test for the 'is_active' method of Derogation queryset

--- home_web/apps/heating/tests/test_models.py
+++ home_web/apps/heating/tests/test_models.py
@@ -26,6 +26,23 @@ class SlotModelTest(TestCase):
 
 class DerogationModelTest(TestCase):
 
+    def create_entries_for_active_tests(self):
+        self.past_derog = Derogation.objects.create(
+            start_dt = timezone.now() - datetime.timedelta(days=1),
+            end_dt = timezone.now() - datetime.timedelta(minutes=2),
+            mode = 'E'
+        )
+        self.active_derog = Derogation.objects.create(
+            start_dt = timezone.now(),
+            end_dt = timezone.now() + datetime.timedelta(minutes=2),
+            mode = 'E'
+        )
+        self.future_derog = Derogation.objects.create(
+            start_dt = timezone.now() + datetime.timedelta(minutes=2),
+            end_dt = timezone.now() + datetime.timedelta(days=1),
+            mode = 'E'
+        )
+
     def test_string_representation(self):
         zone2 = Zone.objects.create(num=2)
         zone3 = Zone.objects.create(num=3)
@@ -39,3 +56,9 @@ class DerogationModelTest(TestCase):
         derog = Derogation.objects.create(start_dt=start, end_dt=end, mode='H')
         derog.zones = [zone2, zone3]
         self.assertEqual(str(derog), "25/02-17:24->18/03-18:12 H Z2-Z3")
+
+    def test_is_active_queryset_method(self):
+        self.create_entries_for_active_tests()
+        queryset = Derogation.objects.is_active()
+        self.assertEqual(queryset.count(), 1)
+        self.assertEqual(queryset[0], self.active_derog)
