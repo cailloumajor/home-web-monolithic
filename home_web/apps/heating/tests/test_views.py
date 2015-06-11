290409b5becef82b900d32f05d9aee954cfa9df9
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-06-11 10:15:18 +0200

    Convert byte type response content to str type

--- home_web/apps/heating/tests/test_views.py
+++ home_web/apps/heating/tests/test_views.py
@@ -67,9 +67,9 @@ class AjaxSlotViewsTest(TestCase):
         url = reverse('new_slot', kwargs={'zone':1})
         response = self.client.post(url, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         response_non_ajax = self.client.post(url)
-        self.assertIsInstance(json.loads(response.content), dict)
-        self.assertEqual(json.loads(response.content),
-                         response_non_ajax.context['form'].errors)
+        response_dict = json.loads(response.content.decode('utf-8'))
+        self.assertIsInstance(response_dict, dict)
+        self.assertEqual(response_dict, response_non_ajax.context['form'].errors)
 
     def test_ajax_slot_create_view_form_valid(self):
         z = Zone.objects.create(num=1, desc="Test zone")
@@ -83,10 +83,10 @@ class AjaxSlotViewsTest(TestCase):
             },
             HTTP_X_REQUESTED_WITH='XMLHttpRequest'
         )
-        self.assertIsInstance(json.loads(response.content), dict)
         created_slot = Slot.objects.all()[0]
         model_dict = model_to_dict(created_slot)
-        response_dict = json.loads(response.content)
+        response_dict = json.loads(response.content.decode('utf-8'))
+        self.assertIsInstance(response_dict, dict)
         for key in model_dict:
             self.assertEqual(
                 response_dict.get(key),
@@ -111,6 +111,7 @@ class AjaxSlotViewsTest(TestCase):
         self.assertEqual(Slot.objects.all().get(), s)
         response = self.client.post(reverse('del_slot', kwargs={'pk':s.pk}),
                                     HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        self.assertIsInstance(json.loads(response.content), dict)
-        self.assertEqual(json.loads(response.content).get('django_success'),
+        response_dict = json.loads(response.content.decode('utf-8'))
+        self.assertIsInstance(response_dict, dict)
+        self.assertEqual(response_dict.get('django_success'),
                          True)
