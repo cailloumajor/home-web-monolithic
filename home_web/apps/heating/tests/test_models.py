be5f3079aeb4170fb3365a9fcd522e2f612f94f4
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-07-06 21:33:23 +0200

    Update model tests to use DDF

--- home_web/apps/heating/tests/test_models.py
+++ home_web/apps/heating/tests/test_models.py
@@ -5,56 +5,52 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 
+from django_dynamic_fixture import G, N, F
+
 from ..models import Zone, Slot, Derogation
 
 class ZoneModelTest(TestCase):
 
     def test_string_representation(self):
-        zone = Zone(num=1)
+        zone = N(Zone)
         self.assertEqual(str(zone), 'Z{}'.format(zone.num))
 
     def test_is_active_queryset_method(self):
-        zone = Zone(num=1)
         wdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
         now = timezone.localtime(timezone.now())
-        if now.time().hour < 1:
-            raise Exception("This test cannot be run between 00:00 and 01:00")
-        z1 = Zone.objects.create(num=1)
-        z2 = Zone.objects.create(num=2)
-        z3 = Zone.objects.create(num=3)
+        z1 = G(Zone, num=1)
         kwargs = {
-            'zone': z2, wdays[now.weekday()]: True, 'mode': 'E',
+            'zone': F(num=2), wdays[now.weekday()]: True, 'mode': 'E',
             'start_time': (now - datetime.timedelta(minutes=2)).time(),
             'end_time': (now + datetime.timedelta(minutes=2)).time(),
         }
-        slot_z2 = Slot.objects.create(**kwargs)
+        slot_z2 = G(Slot, **kwargs)
         kwargs = {
-            'zone': z3, wdays[now.weekday()]: True, 'mode': 'H',
+            'zone': F(num=3), wdays[now.weekday()]: True, 'mode': 'H',
             'start_time': (now - datetime.timedelta(minutes=2)).time(),
             'end_time': (now + datetime.timedelta(minutes=2)).time(),
         }
-        slot_z3 = Slot.objects.create(**kwargs)
-        derog_z3 = Derogation.objects.create(
+        slot_z3 = G(Slot, **kwargs)
+        derog_z3 = G(
+            Derogation, mode = 'A', zones=[F(num=3)],
             start_dt = timezone.now() - datetime.timedelta(minutes=2),
             end_dt = timezone.now() + datetime.timedelta(minutes=2),
-            mode = 'A'
         )
-        derog_z3.zones = [z3]
         self.assertEqual(Zone.objects.get_modes(), {'1':'C','2':'E','3':'A'})
 
 class SlotModelTest(TestCase):
 
     def test_string_representation(self):
-        zone = Zone(num=1)
-        slot = Slot(
-            zone=zone, mon=True, wed=True, fri=True, sun=True,
-            start_time = datetime.time(4, 2), end_time = datetime.time(15, 54),
-            mode = 'E'
+        slot = N(
+            Slot, mon=True, wed=True, fri=True, sun=True, mode = 'E',
+            start_time = datetime.time(4, 2), end_time = datetime.time(15, 54)
         )
-        self.assertEqual(str(slot), "Z1 04:02:00-15:54:00 [L*M*V*D] Eco")
+        self.assertEqual(
+            str(slot), "Z{} 04:02:00-15:54:00 [L*M*V*D] Eco".format(slot.zone.num)
+        )
 
     def test_is_active_queryset_method(self):
-        zone = Zone(num=1)
+        zone = G(Zone, num=1)
         wdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
         now = timezone.localtime(timezone.now())
         if now.time().hour < 1:
@@ -64,31 +60,31 @@ class SlotModelTest(TestCase):
             'start_time': (now - datetime.timedelta(hours=1)).time(),
             'end_time': (now - datetime.timedelta(minutes=2)).time()
         }
-        past_slot = Slot.objects.create(**kwargs)
+        past_slot = G(Slot, **kwargs)
         kwargs = {
             'zone': zone, wdays[now.weekday()]: True, 'mode': 'E',
             'start_time': now.time(),
             'end_time': (now + datetime.timedelta(minutes=2)).time()
         }
-        active_slot = Slot.objects.create(**kwargs)
+        active_slot = G(Slot, **kwargs)
         kwargs = {
             'zone': zone, wdays[now.weekday()]: True, 'mode': 'E',
             'start_time': (now + datetime.timedelta(minutes=2)).time(),
             'end_time': (now - datetime.timedelta(hours=1)).time()
         }
-        future_slot = Slot.objects.create(**kwargs)
+        future_slot = G(Slot, **kwargs)
         kwargs = {
             'zone': zone, wdays[(now.weekday() - 1) % 7]: True, 'mode': 'E',
             'start_time': (now - datetime.timedelta(hours=1)).time(),
             'end_time': (now - datetime.timedelta(minutes=2)).time()
         }
-        yesterday_slot = Slot.objects.create(**kwargs)
+        yesterday_slot = G(Slot, **kwargs)
         kwargs = {
             'zone': zone, wdays[(now.weekday() + 1) % 7]: True, 'mode': 'E',
             'start_time': (now - datetime.timedelta(hours=1)).time(),
             'end_time': (now - datetime.timedelta(minutes=2)).time()
         }
-        tomorrow_slot = Slot.objects.create(**kwargs)
+        tomorrow_slot = G(Slot, **kwargs)
         queryset = Slot.objects.is_active()
         self.assertEqual(queryset.count(), 1)
         self.assertEqual(queryset[0], active_slot)
@@ -96,25 +92,23 @@ class SlotModelTest(TestCase):
 class DerogationModelTest(TestCase):
 
     def create_entries_for_active_tests(self):
-        self.past_derog = Derogation.objects.create(
+        self.past_derog = G(
+            Derogation, mode = 'E',
             start_dt = timezone.now() - datetime.timedelta(days=1),
-            end_dt = timezone.now() - datetime.timedelta(minutes=2),
-            mode = 'E'
+            end_dt = timezone.now() - datetime.timedelta(minutes=2)
         )
-        self.active_derog = Derogation.objects.create(
+        self.active_derog = G(
+            Derogation, mode = 'E',
             start_dt = timezone.now(),
-            end_dt = timezone.now() + datetime.timedelta(minutes=2),
-            mode = 'E'
+            end_dt = timezone.now() + datetime.timedelta(minutes=2)
         )
-        self.future_derog = Derogation.objects.create(
+        self.future_derog = G(
+            Derogation, mode = 'E',
             start_dt = timezone.now() + datetime.timedelta(minutes=2),
-            end_dt = timezone.now() + datetime.timedelta(days=1),
-            mode = 'E'
+            end_dt = timezone.now() + datetime.timedelta(days=1)
         )
 
     def test_string_representation(self):
-        zone2 = Zone.objects.create(num=2)
-        zone3 = Zone.objects.create(num=3)
         tz = timezone.get_default_timezone()
         start = timezone.make_aware(
             datetime.datetime(2015, 2, 25, 17, 24), tz
@@ -122,8 +116,8 @@ class DerogationModelTest(TestCase):
         end = timezone.make_aware(
             datetime.datetime(2015, 3, 18, 18, 12), tz
         )
-        derog = Derogation.objects.create(start_dt=start, end_dt=end, mode='H')
-        derog.zones = [zone2, zone3]
+        derog = G(Derogation, start_dt=start, end_dt=end, mode='H',
+                  zones=[F(num=2), F(num=3)])
         self.assertEqual(str(derog), "25/02-17:24->18/03-18:12 H Z2-Z3")
 
     def test_is_active_queryset_method(self):
