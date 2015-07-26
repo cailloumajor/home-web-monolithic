a1f55a3d27047226e250012602029fb62f443b7c
Arnaud Rocher <arnaud.roche3@gmail.com>
2015-07-26 22:21:46 +0200

    Move app directory at project root

--- home_web/settings/development.py
+++ home_web/settings/development.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home_web.apps.heating',
+    'heating',
 )
 
 MIDDLEWARE_CLASSES = (
--- home_web/urls.py
+++ home_web/urls.py
@@ -8,5 +8,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^heating/', include('home_web.apps.heating.urls')),
+    url(r'^heating/', include('heating.urls')),
 ]
